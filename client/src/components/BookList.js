2c7b57d62860cb8367df8f363acde0f0196b5e82
gkaragoz <gkaragoz96@gmail.com>
2020-08-19 11:57:21 +0300

    Implement Apollo Client and request bookList.

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 function BookList() {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if (loading) return <div>Loading books...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  console.log(data);
+
+  const displayBooks = () => {
+    return data.books.map((book) => {
+      return <li key={book.id}>{book.name}</li>;
+    });
+  };
+
   return (
     <div>
-      <ul id="book-list">
-        <li>Book name</li>
-      </ul>
+      <ul id="book-list">{displayBooks()}</ul>
     </div>
   );
 }
