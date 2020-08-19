d4e20a3004b1d3ef28bc783bc1afc9bcf017dc4c
gkaragoz <gkaragoz96@gmail.com>
2020-08-19 11:57:04 +0300

    Install cors

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
